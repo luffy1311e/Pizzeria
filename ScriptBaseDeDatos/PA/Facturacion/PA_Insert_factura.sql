df5ad06705fe14d8fde70b8259ba019adec70c33
Edgar Romero <edgarjromero@outlook.com>
2015-08-07 20:46:49 -0600

    sp detalle

--- ScriptBaseDeDatos/PA/Facturacion/PA_Insert_factura.sql
+++ ScriptBaseDeDatos/PA/Facturacion/PA_Insert_factura.sql
@@ -9,35 +9,46 @@ drop procedure if exists PA_I_Factura;
 
 DELIMITER //
 
-CREATE PROCEDURE PA_I_Factura(pNumeroFac int, PFecha datetime, total decimal, ivi decimal,  ptipoPago int,  INOUT pMensajeError VARCHAR(2000))
+CREATE PROCEDURE PA_I_Factura(
+	PFecha datetime,
+    pTotal decimal,
+    pIvi decimal,
+    pTipoPago int,
+    out msg_error VARCHAR(100),
+    out pId int)
+    
 bloquePrincipal:
 BEGIN
      
-   -- Declaración de variables locales
-   DECLARE vCantidad_Registros INT;
-   DECLARE vError INT;
-   DECLARE cNombre_Logica VARCHAR(30) DEFAULT 'Lógica [PA_I_Factura]';
-
-   -- Declaración de bloque con Handler para manejo de SQLException
-   DECLARE EXIT HANDLER FOR SQLEXCEPTION, SQLWARNING
-   Handler_SqlException:
-   BEGIN
-      ROLLBACK;
-      SET pMensajeError = CONCAT('Ocurrió un error al ejecutar el procedimiento. ', cNombre_Logica);
-	  LEAVE Handler_SqlException;
-   END; 
-
-   -- Declaración de inicio de Transacción - @@autocommit = 0
-   START TRANSACTION;
-   
-   -- Asignaciones de valores a variables locales
-   SET vCantidad_Registros = 0;
-   SET pMensajeError = "";
+	-- Declaración de variables locales
+	declare nombre_PA varchar(15) default '[PA_I_Factura]';
+	declare cantidad_registros int;
+	declare error_sql int;
+    
+	-- Declaracion del handler para el manejo de excepciones
+	declare exit handler for sqlexception, sqlwarning
+	handler_exception:
+	begin
+		set msg_error = concat('Ocurrio un error al ejecutar el procedimiento: ', nombre_PA);
+		leave handler_exception;
+	end; -- handler_exception
    
    -- Insertar en la tabla de Factura
-	INSERT INTO Factura (numerofac, fecha, total, ivi, tipopago, estado)
-	VALUES (pNumeroFac,PFecha,total,ptipoPago,ivi,1);
-
-	COMMIT;
-end
+	INSERT INTO Factura (fecha, total, ivi, tipopago, activo)
+	VALUES (PFecha,pTotal, pIvi,pTipoPago);
+    
+    -- @@error_count Contiene la cantidad de errores ocurridos en la transacción
+	set error_sql = (select @@error_count);
+    
+    -- Evaluar si hay errores o no se afectaron tuplas en la transacción
+	-- La función ROW_COUNT() retorna la cantidad de filas afectadas en la última sentencia INSERT/UPDATE/DELETE ejecutada
+	if (error_sql > 0 OR ROW_COUNT() <= 0) then
+		rollback;
+		set msg_error = CONCAT('Ocurrió un error al ejecutar el procedimiento ', nombre_PA, '. No se insertó el registro a la factura.');
+	else
+		select max(id) into pID
+		from Pizza;
+	end if;
+    
+end; -- bloquePrincipal
 //
